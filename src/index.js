4a5c437197b67832873a7aad494629187d0cb764
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-10-06 19:45:36 +0300

    Create index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-document.getElementById('root').innerHTML = 'HEllo WOrld!';
+import './index.scss';
+import App from './App';
+import { mount } from '../utils';
+
+const tabCtrl = (() => {
+  const handlers = [];
+  const subscribe = handler => {
+    handlers.push(handler);
+  };
+  const fire = e => {
+    handlers.forEach(handler => handler(e));
+  };
+  return {
+    fire,
+    subscribe,
+  };
+})();
+
+mount(App(tabCtrl), document.getElementById('root'));
